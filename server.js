b8cf4bde58b53164080bfb71a9f61c9aef98ec4c
Justin Goats <justingoats@yahoo.com>
2021-08-19 22:56:11 -0700

    getting started using ejs template engine

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require("express");
 let app = express();
+let router = express.Router();
+
+app.set("view engine", "ejs");
+// specifying that any routes created here will be behind "/articles"
+app.use("/articles", router);
 
 app.get("/", (req, res) => {
-    res.send("welcome to my main page");
+    const articles = [{
+        title: "my first blog",
+        description: "i love coding",
+        content: "jim flew over the baron"
+    }]
+    res.render("index", { articles: articles });
+})
+router.get("/", (req, res) => {
+    res.send("in articles");
 })
 
 app.listen(3000, () => {
