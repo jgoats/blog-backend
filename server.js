52e9c95aac5b64a3deca6ce7acd5e5b1c848c308
Justin Goats <justingoats@yahoo.com>
2021-09-23 22:28:09 -0700

    commit to master

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var cookieParser = require('cookie-parser');
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: true,
     credentials: true
 }))
 mongoose.connect(process.env.CONNECTION_STRING,
