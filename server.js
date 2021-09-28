bcf5aab42b84ce87058910cf4bcd77b937c5e9aa
Justin Goats <justingoats@yahoo.com>
2021-09-28 15:06:20 -0700

    removed header middleware for backend

--- server.js
+++ server.js
@@ -36,12 +36,13 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({ storage: storage }).single('file');
+/*
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://www.justinssoftware.com");
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});
+});*/
 app.get("/", (req, res) => {
     res.send("welcome to blog-backend");
 })
